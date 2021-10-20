af982039d26c0dfb482be1ec2f57c78496b79b0a
Mitchell Henke <mitchell.henke@gsa.gov>
2021-10-20 13:49:30 -0500

    only return CSP hosts instead of all redirect uris to simplify

--- app/services/secure_headers_allow_list.rb
+++ app/services/secure_headers_allow_list.rb
@@ -6,7 +6,12 @@ class SecureHeadersAllowList
   def self.csp_with_sp_redirect_uris(action_url_domain, sp_redirect_uris)
     csp_uris = ["'self'", action_url_domain]
 
-    csp_uris |= sp_redirect_uris.compact if sp_redirect_uris.present?
+    sp_redirect_hosts = Array(sp_redirect_uris).map do |redirect_uri|
+      uri = URI.parse(redirect_uri)
+      "#{uri.scheme}://#{uri.host}:#{uri.port}"
+    end.compact.uniq
+
+    csp_uris |= sp_redirect_hosts if sp_redirect_hosts.present?
 
     csp_uris
   end
